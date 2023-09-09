04abbb473205a0692b0cf37e111de1d047b9895f
wubinben <wubinben@icloud.com>
2023-09-09 21:51:52 +0800

    chore: create a blank project from start.spring.io

---
+++ src/main/java/com/wuzhenben/todoapi/TodoapiApplication.java
@@ -0,0 +1,13 @@
+package com.wuzhenben.todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoapiApplication.class, args);
+	}
+
+}
