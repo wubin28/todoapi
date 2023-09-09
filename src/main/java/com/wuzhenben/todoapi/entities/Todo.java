75069666262e45091c36f8d6a6f977212b23d6eb
wubinben <wubinben@icloud.com>
2023-09-10 00:05:46 +0800

    chore: add entity todo

---
+++ src/main/java/com/wuzhenben/todoapi/entities/Todo.java
@@ -0,0 +1,25 @@
+package com.wuzhenben.todoapi.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+@Table(name = "todo")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "completed")
+    private Boolean completed;
+
+}
